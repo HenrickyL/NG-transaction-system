4d54c6b455a4d9ab428acd5dc7fa3b8d2ce986b7
HenrickyL <Henrickyl1@gmail.com>
2022-11-19 08:02:19 -0300

    docker config

--- server/app/infra/http/server.ts
+++ server/app/infra/http/server.ts
@@ -1,17 +1,28 @@
 import cors from 'cors'
 import express from 'express';
+import {createServer} from 'http'
 import { router } from './routes';
 import {port} from '@config/auth'
-const server = express();
 
-server.use(
-  cors({
-    exposedHeaders: ['x-total-count', 'Content-Type', 'Content-Length'],
+const api = express();
+const http = createServer(api)
+const currentPort = port || 3333
+
+api.use(express.urlencoded({ extended : true }))
+api.use(express.json());
+api.use(cors())
+api.use(router);
+api.set('port', currentPort);
+
+
+api.get('/',(req,res)=>{
+ return res.status(200).json({
+    message: 'deu certo'
   })
-)
-server.use(express.json());
-server.use(router);
-const currentPort = port || 3334
-server.listen( currentPort, () => {
-  console.log(`Server running on http://localhost:${currentPort}`);
+})
+
+
+
+http.listen( currentPort, () => {
+  console.log(`Server running on port ${currentPort}`);
 });
---
+++ web/README.md
@@ -0,0 +1,16 @@
+# NG.CASH - Front
+
+* React
+* typescript
+* tailwind css
+* storybook doc
+
+
+* build docker here:
+```sh
+docker build -f Dockerfile.dev -t multiserver .
+```
+* run:
+```
+docker run -it -p 4003:5000 multiserver
+```
