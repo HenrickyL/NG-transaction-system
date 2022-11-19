ed52126353b3823fcbdc0326eb714961e0f2bfba
HenrickyL <Henrickyl1@gmail.com>
2022-11-19 01:36:14 -0300

    feat: add theme config in taiwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        xs:14,
+        sm:16,
+        md:18,
+        lg:20,
+        xl:24,
+        '2xl':32
+      },
+      colors:{
+        trasparent:'transparent',
+        black: '#000',
+        white: '#fff',
+        gray:{
+          100: '#e1e1e6',
+          200: '#c4c4cc',
+          400: '#7c7c8a',
+          800: '#202024',
+          900: '#121214',
+        },
+        cyan:{
+          300: '#98e1f8',
+          500: '#81d8f7',
+        }
+      },
+      fontFamily: {
+        sans: "Inter, sans-serif"
+      }
+    },
   },
   plugins: [],
 }
