19a4569f880bdc582ae8a09a35aed7dead790091
HenrickyL <Henrickyl1@gmail.com>
2022-11-19 11:53:13 -0300

    feat: create abstract entity class

---
+++ server/app/core/domain/Entity.ts
@@ -0,0 +1,33 @@
+import { v4 as uuid } from 'uuid'
+export abstract class Entity<T>{
+
+  protected readonly _id: string
+  public readonly props: T
+
+  constructor(props: T, id?: string) {
+    this._id = id || uuid()
+    this.props = props
+  }
+
+  get id() {
+    return this._id
+  }
+
+  public equals(object?: Entity<T>): boolean {
+    if (object === null || object === undefined) {
+      return false
+    }
+
+    if (this === object) {
+      return true
+    }
+
+    if (!(object instanceof Entity)) {
+      return false
+    }
+
+    return this._id === object._id
+  }
+
+
+}
