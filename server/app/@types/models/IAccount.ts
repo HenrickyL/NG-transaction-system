4a89c855c90abed44e8e8161c899f98db5a85401
HenrickyL <Henrickyl1@gmail.com>
2022-11-20 06:37:32 -0300

    feat: remove old models (entity)

--- server/app/@types/models/IAccount.ts
+++
@@ -1,7 +0,0 @@
-import { IEntity } from ".";
-import { IUser } from 'app/@types/models';
-
-export interface IAccount extends IEntity{
-  balance: number
-  user?: IUser
-}
--- server/app/@types/models/ITransaction.ts
+++
@@ -1,9 +0,0 @@
-import { IEntity } from ".";
-import { IAccount } from './IAccount';
-
-export interface ITransaction extends IEntity{
-  value: number
-  debitedAccountId: string
-  debitedAccount?:    IAccount  
-  creditedAccount?:   IAccount
-}
