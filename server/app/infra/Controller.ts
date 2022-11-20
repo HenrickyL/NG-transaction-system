c1c0409471dcd54629bbc22552e678ef3e768efd
HenrickyL <Henrickyl1@gmail.com>
2022-11-20 07:07:02 -0300

    chore: types config

--- server/app/infra/Controller.ts
+++ server/app/infra/Controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from "types/HttpResponses";
+import { HttpResponse } from "app/_types/HttpResponses";
 
 export abstract class Controller<T>{
   handle: (request: T) => Promise<HttpResponse<T>>
