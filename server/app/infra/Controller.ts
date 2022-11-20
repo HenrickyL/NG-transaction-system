eb49fbe8a51d6c646e4dde1d01d135f1f6c098fb
HenrickyL <Henrickyl1@gmail.com>
2022-11-20 07:05:31 -0300

    config types & convert to DDD (initial)

--- server/app/infra/Controller.ts
+++ server/app/infra/Controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from "app/@types/responses";
+import { HttpResponse } from "types/HttpResponses";
 
 export abstract class Controller<T>{
   handle: (request: T) => Promise<HttpResponse<T>>
