07d40f2ed471d26af5648c9e866ddcb3b2514a06
HenrickyL <Henrickyl1@gmail.com>
2022-11-22 19:58:56 -0300

    feat: GetBalanceController

---
+++ server/app/Modules/account/UseCases/GetBalance/GetBalanceController.ts
@@ -0,0 +1,19 @@
+
+import { IController } from '@core/infra/IController';
+import { HttpResponse, ok } from 'types/HttpResponses';
+import { GetBalance } from './GetBalance';
+import { AccountGetBalanceRequest, AccountResponse } from 'types/DTOs/accountDTO';
+
+export class GetBalanceController implements IController{
+  constructor(
+    private getBalance: GetBalance
+  ) {}
+  
+  async handle (request: AccountGetBalanceRequest): Promise<HttpResponse<AccountResponse>>{
+
+      const result = await this.getBalance.execute(request)
+      return ok(result)
+
+  }
+
+}
