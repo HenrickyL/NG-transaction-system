43b82478aad4320f6bac43f58f9884861d412516
HenrickyL <Henrickyl1@gmail.com>
2022-11-24 09:04:14 -0300

    feat: create router

--- server/app/infra/http/Routes/account.routes.ts
+++ server/app/infra/http/Routes/account.routes.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { makeCashOutController } from "../factories/controllers/CashOutControllerFactory";
 import { makeGetBalanceController } from "../factories/controllers/GetBalanceControllerFactory";
 import { makeEnsureAuthenticatedMiddleware } from "../factories/middlewares/EnsureAuthenticatedMiddlewareFactory";
+import { makeGetTransactionsController } from './../factories/controllers/GetTransactionsControllerFactory';
 
 
 const accountRouter = express.Router()
@@ -11,5 +12,5 @@ const accountRouter = express.Router()
 accountRouter.use(adaptMiddleware(makeEnsureAuthenticatedMiddleware()))
 accountRouter.get('/:userId', adaptRoute(makeGetBalanceController()))
 accountRouter.post('/:userId', adaptRoute(makeCashOutController()))
-
+accountRouter.get('/:userId/transactions', adaptRoute(makeGetTransactionsController()))
 export { accountRouter }
---
+++ server/app/infra/http/factories/controllers/GetTransactionsControllerFactory.ts
@@ -0,0 +1,18 @@
+import { IController } from "@core/infra/IController";
+import { TransactionMapper } from "@modules/transactions/mapper/TransactionMapper";
+import { PrismaTransactionRepository } from "@modules/transactions/repositories/prisma/PrismaITransactionRepository";
+import { UserMapper } from "@modules/user/mapper/UserMapper";
+import { PrismaUsersRepository } from "@modules/user/repositories/prisma/PrismaUserRepository";
+import { TransactionRequest, GetTransactionResponse } from "types/DTOs/transactionDTO";
+import { GetTransactions } from './../../../../Modules/account/UseCases/GetTransactions/GetTransactions';
+import { GetTransactionsController } from './../../../../Modules/account/UseCases/GetTransactions/GetTransactionsController';
+
+export function makeGetTransactionsController(): IController<TransactionRequest,GetTransactionResponse> {
+  const userMapper = new UserMapper()
+  const transactionMapper = new TransactionMapper()
+  const prismaUsersRepository = new PrismaUsersRepository(userMapper)
+  const prismaTransactionRepository = new PrismaTransactionRepository(transactionMapper)
+  const useCase = new GetTransactions(prismaUsersRepository, prismaTransactionRepository,transactionMapper);
+  const controller = new GetTransactionsController(useCase)
+  return controller
+}
