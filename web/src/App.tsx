e260c93e159286bd68db698e61466ec7a49f769f
HenrickyL <Henrickyl1@gmail.com>
2022-11-24 22:15:30 -0300

    chore: config web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,6 @@
 import './styles/global.css'
-import axios from 'axios'
-import { useEffect } from 'react'
-import {port,domain} from './@types/auth'
-
 export default () => {
-  useEffect(() => {
-    axios
-      .get(`http://${domain}:${port}`)
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(console.error)
-  }, [])
-
+ 
   return (
     <div>Hello World</div>
   )
